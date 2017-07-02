47ad8be7fd85ed77b2459cece46d49f7d9099310
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-03 00:39:53 +0530

    Working response on Submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 //import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
+
 export default class App extends Component {
 
   constructor(props, context) {
     super(props, context);
-    this.state = { uname: '', pswd: '' };
+    this.state = { uid: '', password: '' };
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onFormChange = this.onFormChange.bind(this);
   }
@@ -15,40 +16,39 @@ export default class App extends Component {
     this.setState({
       [e.target.name]: e.target.value,
     });
-    //console.log("Value: " + e.target.value);
+    //console.log("Value: "+ e.target.name + e.target.value);
   }
 
   onFormSubmit(e) {
-  // e.preventDefault();
+    e.preventDefault();
+    console.log(this.state);
 
-    // fetch(this.props.formAction, {
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({uname: this.state.uname, pswd: this.state.pswd})
-    // });
-    //
-    // this.setState({uname: '', pswd: ''});
-    //componentDidMount(){
-
-        var url = "http://api.brime.ml/user/login";
-        var config = {};
-        config.headers = {
-            'Accept': 'application/json',
-            "Content-Type" : "application/json"
-        };
-
-        axios.get(url, config).then(
-            function(response){
-                console.log(response);
-
-                JSON.stringify({uname: this.state.uname, pswd: this.state.pswd})
-                //var events = response.data;
-                this.setState({uname: '', pswd: ''});
-            }.bind(this))
-    }
-
+    axios({
+      url: this.props.action,
+      method: 'post',
+      headers: {
+        'Accept':'application/json'
+      },
+      data: {
+        uid: this.state.uid,
+        password: this.state.password
+      }
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+    this.setState({ uid: '', password: '' });
+  }
 
   render() {
     return (
@@ -58,23 +58,23 @@ export default class App extends Component {
           action={this.props.action}
           method={this.props.method}
           onSubmit={this.onFormSubmit}>
-            <h2>Login Page</h2>
-            <div className="container">
-              <label><b>Username</b></label><br/>
-              <input type="text" placeholder="Enter Username" name="uname"
-              required onChange={this.onFormChange}/><br/>
+          <h2>Login Page</h2>
+          <div className="container">
+            <label><b>Username</b></label><br />
+            <input type="text" placeholder="Enter Username" name="uid"
+              required onChange={this.onFormChange} /><br />
 
-              <label><b>Password</b></label><br/>
-              <input type="password" placeholder="Enter Password" name="pswd"
-               required onChange={this.onFormChange}/><br/>
+            <label><b>Password</b></label><br />
+            <input type="password" placeholder="Enter Password" name="password"
+              required onChange={this.onFormChange} /><br />
 
-              <button type="submit">Login</button>
-            </div>
+            <button type="submit">Login</button>
+          </div>
 
-            <div className="container" style={{backgroundColor:"#f1f1f1"}}>
-              <a href=""><span className="psw">Forgot password?</span></a><br></br>
-              New User? <a href="">Create account.</a>
-            </div>
+          <div className="container" style={{ backgroundColor: "#f1f1f1" }}>
+            <a href=""><span className="psw">Forgot password?</span></a><br></br>
+            New User? <a href="">Create account.</a>
+          </div>
 
         </form>
       </div>
@@ -82,7 +82,7 @@ export default class App extends Component {
   }
 }
 
- App.defaultProps = {
-   action: 'http://api.brime.ml/user/login',
-//   method: 'get'
- };
+App.defaultProps = {
+  action: 'http://brime.ml/user/login',
+  //  method: 'post'
+};
